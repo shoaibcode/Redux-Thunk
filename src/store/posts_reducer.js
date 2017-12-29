490a82c1195a62476d57149f1bec9e2ccb5cb861
shoaibbhimani <shoaibbhimani1392@gmail.com>
2017-12-30 03:46:45 +0530

    Redux Thunk Tuts Complete

---
+++ src/store/posts_reducer.js
@@ -0,0 +1,12 @@
+const todosReducer = (state = [], action) => {
+	switch (action.type) {
+		case "GET_POSTS":
+		 return [...action.payload]
+		default:
+			return state;
+	}
+};
+
+export default todosReducer;
+
+
